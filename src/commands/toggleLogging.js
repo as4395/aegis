63cb968acd3f3600130f2c594962f9808cdfb38a
Abhiram <notifysec@proton.me>
2025-07-26 22:47:13 -0400

    Update toggleLogging.js

--- src/commands/toggleLogging.js
+++ src/commands/toggleLogging.js
@@ -6,7 +6,7 @@ export const data = new SlashCommandBuilder()
   .setDescription('Enable or disable logging for this server.')
   .addBooleanOption(opt =>
     opt.setName('enabled')
-      .setDescription('Set to true to enable, false to disable')
+      .setDescription('True to enable, false to disable')
       .setRequired(true)
   )
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
@@ -14,6 +14,7 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
   const enabled = interaction.options.getBoolean('enabled');
   setLogging(interaction.guildId, enabled);
+
   await interaction.reply({
     content: `📓 Logging is now **${enabled ? 'enabled' : 'disabled'}**.`,
     ephemeral: true
