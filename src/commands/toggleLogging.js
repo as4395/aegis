9a057f6cac9dc1a258218c2450748a9d7423bf2d
Abhiram <notifysec@proton.me>
2025-07-26 22:43:30 -0400

    Create toggleLogging.js

---
+++ src/commands/toggleLogging.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { setLogging } from '../utils/logger.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('togglelogging')
+  .setDescription('Enable or disable logging for this server.')
+  .addBooleanOption(opt =>
+    opt.setName('enabled')
+      .setDescription('Set to true to enable, false to disable')
+      .setRequired(true)
+  )
+  .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+
+export async function execute(interaction) {
+  const enabled = interaction.options.getBoolean('enabled');
+  setLogging(interaction.guildId, enabled);
+  await interaction.reply({
+    content: `📓 Logging is now **${enabled ? 'enabled' : 'disabled'}**.`,
+    ephemeral: true
+  });
+}
